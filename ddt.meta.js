65c97845f9b204825526d674859542e84738363f
OpenShift guest <ibuchta@yandex.ru>
2014-04-19 20:23:35 -0400

    Init.

---
+++ ddt.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         DesuDesuTalk
+// @namespace    udp://desushelter/*
+// @version      0.0.10
+// @description  Write something useful!
+// @match        http://dobrochan.com/*/res/*
+// @match        http://dobrochan.ru/*/res/*
+// @match        http://dobrochan.org/*/res/*
+// @match        http://inach.org/*/res/*
+// @match        https://8chan.co/*/res/*
+// @match        http://2ch.hk/*/res/*
+// @match        http://iichan.hk/*/res/*
+// @include      http://dobrochan.com/*/res/*
+// @include      http://dobrochan.ru/*/res/*
+// @include      http://dobrochan.org/*/res/*
+// @include      http://inach.org/*/res/*
+// @include      https://8chan.co/*/res/*
+// @include      http://2ch.hk/*/res/*
+// @include      http://iichan.hk/*/res/*
+// @copyright    2014+, Boku 
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
