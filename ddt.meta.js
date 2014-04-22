2bf65a7ac6de8d8e6a8997ff071d60125ed68be8
desudesutalk <souseiseki.desu@yandex.ru>
2014-04-22 10:14:28 -0700

    version bump

--- ddt.meta.js
+++ ddt.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DesuDesuTalk
 // @namespace    udp://desushelter/*
-// @version      0.0.17
+// @version      0.0.18
 // @description  Write something useful!
 // @match        http://dobrochan.com/*/res/*
 // @match        http://dobrochan.ru/*/res/*
