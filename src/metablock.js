e43959231b5a90e0ee09b0d0d542ac9653057ea3
bokufag <souseiseki.desu@yandex.ru>
2015-01-01 20:01:03 +0300

    Form font size fix

--- src/metablock.js
+++ src/metablock.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DesuDesuTalk
 // @namespace    udp://desushelter/*
-// @version      0.4.20
+// @version      0.4.21
 // @description  Write something useful!
 // @include      http://dobrochan.com/*/*
 // @include      http://dobrochan.ru/*/*
