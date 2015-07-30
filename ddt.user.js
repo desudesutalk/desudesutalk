440a6800913c20c850e910b8143d9a81b9500b8b
bokufag <souseiseki.desu@yandex.ru>
2015-07-30 10:21:56 +0300

    Version bump

--- ddt.user.js
+++ ddt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DesuDesuTalk
 // @namespace    udp://desushelter/*
-// @version      0.4.49
+// @version      0.4.50
 // @description  Write something useful!
 // @include      *://dobrochan.com/*/*
 // @include      *://dobrochan.ru/*/*
