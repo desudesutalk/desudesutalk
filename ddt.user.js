ef49668e17d721e3ff716954b2323a947aa73274
bokufag <souseiseki.desu@yandex.ru>
2016-11-26 05:24:32 +0300

    Remove Scriptish trash. Fixes #25

--- ddt.user.js
+++ ddt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DesuDesuTalk
 // @namespace    udp://desushelter/*
-// @version      0.4.82
+// @version      0.4.83
 // @description  Write something useful!
 // @include      *://dobrochan.com/*/*
 // @include      *://dobrochan.ru/*/*
@@ -2789,7 +2789,7 @@ var inject_ui = function() {
                         '<span style="color: #900">De</span>'+
                         '<span style="color: #090">su</span> '+
                         '<span style="color: #900">De</span>'+ /*jshint newcap: false  */
-                        '<span style="color: #090">su</span> Talk!<span style="font-size: x-small;">&nbsp;(v'+(typeof GM_info !== 'undefined' ? GM_info.script.version : GM_getMetadata("version"))+')</span></h3>'+
+                        '<span style="color: #090">su</span> Talk!<span style="font-size: x-small;">&nbsp;(v'+(typeof GM_info !== 'undefined' ? GM_info.script.version : '')+')</span></h3>'+
                     '<div class="hidbord_nav">'+
             '            <div class="hidbord_clickable active" id="hidbord_show_msgs">Messages</div>'+
             '            <div class="hidbord_clickable" id="hidbord_show_cntc">Contacts</div>'+
--- ddt_mobil.user.js
+++ ddt_mobil.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DesuDesuTalk
 // @namespace    udp://desushelter/*
-// @version      0.4.82
+// @version      0.4.83
 // @description  Write something useful!
 // @include      *://dobrochan.com/*/*
 // @include      *://dobrochan.ru/*/*
@@ -41,7 +41,7 @@
 // @exclude      *#dev
 // @copyright    2014+, Boku
 // @icon         https://github.com/desudesutalk/desudesutalk/raw/master/icon.jpg
-// @updateURL    https://github.com/desudesutalk/desudesutalk/raw/master/ddt.meta.js
+// @updateURL    https://github.com/desudesutalk/desudesutalk/raw/master/ddt_mobil.user.js
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
@@ -2789,7 +2789,7 @@ var inject_ui = function() {
                         '<span style="color: #900">De</span>'+
                         '<span style="color: #090">su</span> '+
                         '<span style="color: #900">De</span>'+ /*jshint newcap: false  */
-                        '<span style="color: #090">su</span> Talk!<span style="font-size: x-small;">&nbsp;(v'+(typeof GM_info !== 'undefined' ? GM_info.script.version : GM_getMetadata("version"))+')</span></h3>'+
+                        '<span style="color: #090">su</span> Talk!<span style="font-size: x-small;">&nbsp;(v'+(typeof GM_info !== 'undefined' ? GM_info.script.version : '')+')</span></h3>'+
                     '<div class="hidbord_nav">'+
             '            <div class="hidbord_clickable active" id="hidbord_show_msgs">Messages</div>'+
             '            <div class="hidbord_clickable" id="hidbord_show_cntc">Contacts</div>'+
